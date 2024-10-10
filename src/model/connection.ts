cdb07f007288b0b5bd9fa947b39199642c11ad64
Diego Alisson Monteiro <diegoalisson@pe.senac.br>
2024-10-10 16:05:53 -0300

    fix: connectionString

--- src/model/connection.ts
+++ src/model/connection.ts
@@ -2,9 +2,15 @@ const Sequelize = require('sequelize')
 
 require('dotenv').config({ path: process.env.DEVMODE?.trim() === 'test' ? '.env.test' : '.env' })
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PWD, {
-  host: process.env.SERVER,
-  dialect: process.env.DIALECT,
+// const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PWD, {
+//   host: process.env.SERVER,
+//   dialect: process.env.DIALECT,
+//   logging: false
+// })
+
+const sequelize = new Sequelize('MigracaoZum', 'sa', 'local', {
+  host: '10.9.8.74',
+  dialect: 'mssql',
   logging: false
 })
 
